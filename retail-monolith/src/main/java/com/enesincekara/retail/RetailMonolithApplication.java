ffd23406d71f15944f75ca5cffa9613ca5cca538
enesincekaraa <enesincekara61@gmail.com>
2026-03-15 13:57:46 +0300

    initial platform setup

---
+++ retail-monolith/src/main/java/com/enesincekara/retail/RetailMonolithApplication.java
@@ -0,0 +1,13 @@
+package com.enesincekara.retail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailMonolithApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetailMonolithApplication.class, args);
+    }
+
+}
---
+++ retail-monolith/src/test/java/com/enesincekara/retail/RetailMonolithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enesincekara.retail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailMonolithApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
