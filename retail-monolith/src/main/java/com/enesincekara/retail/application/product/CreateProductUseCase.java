e601fc9aaa03db160cb1cd38dcb2e202b8e1a2bc
enesincekaraa <enesincekara61@gmail.com>
2026-03-16 13:50:29 +0300

    add db config and docker-compose

--- retail-monolith/src/main/java/com/enesincekara/retail/application/product/CreateProductUseCase.java
+++ retail-monolith/src/main/java/com/enesincekara/retail/application/product/CreateProductUseCase.java
@@ -3,7 +3,9 @@ package com.enesincekara.retail.application.product;
 import com.enesincekara.retail.domain.product.Product;
 import com.enesincekara.retail.domain.product.ProductRepository;
 import com.enesincekara.retail.domain.shared.Money;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CreateProductUseCase {
     private final ProductRepository productRepository;
 
