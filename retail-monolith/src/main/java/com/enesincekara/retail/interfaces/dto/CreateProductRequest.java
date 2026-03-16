cf9dc45245faabd4d6cb9fd5f12598bd824a1f8f
enesincekaraa <enesincekara61@gmail.com>
2026-03-16 12:50:41 +0300

    add validation to create product request

--- retail-monolith/src/main/java/com/enesincekara/retail/interfaces/dto/CreateProductRequest.java
+++ retail-monolith/src/main/java/com/enesincekara/retail/interfaces/dto/CreateProductRequest.java
@@ -1,10 +1,17 @@
 package com.enesincekara.retail.interfaces.dto;
 
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 import java.math.BigDecimal;
 
 public class CreateProductRequest {
+    @NotBlank(message = "Product name cannot be blank")
     private String  name;
+    @NotNull(message = "Price cannot be null")
+    @Positive(message = "Price must be greater than zero")
     private BigDecimal price;
 
     public String getName() {
